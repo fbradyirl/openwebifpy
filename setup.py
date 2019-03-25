9539d1cd3b50029692e2bd778ad8039b7c85ef25
Finbarr Brady <finbarr.brady@gmail.com>
2019-03-25 15:19:24 +0000

    Bump version: 3.0.6 → 3.0.7

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/openwebifpy',
-    version='3.0.6',
+    version='3.0.7',
     zip_safe=False,
 )
