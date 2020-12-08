840ec5616a28dcfb7c5e93274ad781d6014585f5
Finbarr Brady <fibrady@cisco.com>
2020-12-08 14:37:25 +0000

    Bump version: 3.1.1 → 3.1.2

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/openwebifpy',
-    version='3.1.1',
+    version='3.1.2',
     zip_safe=False,
 )
